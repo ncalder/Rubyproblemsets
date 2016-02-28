d33b691b0b88b30da533e17ac213ecb84fa4aa12
Nikolai <nikolaicalder1@gmail.com>
2016-02-28 04:50:56 -0500

    added solution

---
+++ sum_nums.rb
@@ -0,0 +1,14 @@
+def sum_nums(n)
+	i = 0 
+	result = 0 
+	while i <= n 
+		result += i
+		i += 1
+	end
+	p result
+end
+puts('sum_nums(1) == 1: ' + (sum_nums(1) == 1).to_s)
+puts('sum_nums(2) == 3: ' + (sum_nums(2) == 3).to_s)
+puts('sum_nums(3) == 6: ' + (sum_nums(3) == 6).to_s)
+puts('sum_nums(4) == 10: ' + (sum_nums(4) == 10).to_s)
+puts('sum_nums(5) == 15: ' + (sum_nums(5) == 15).to_s)
