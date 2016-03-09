cdb43ff02fe5a9b5ac5c343dddbe55ca2681595b
Nikolai <nikolaicalder1@gmail.com>
2016-03-09 07:24:51 -0500

    solved

---
+++ greatfactor.rb
@@ -0,0 +1,19 @@
+def greatest_common_factor(num1,num2)  
+  num1, num2 = num2, num1%num2  until num2 == 0   
+  num1  
+end
+
+
+
+puts(
+  'greatest_common_factor(3, 9) == 3: ' +
+  (greatest_common_factor(3, 9) == 3).to_s
+)
+puts(
+  'greatest_common_factor(16, 24) == 8: ' +
+  (greatest_common_factor(16, 24) == 8).to_s
+)
+puts(
+  'greatest_common_factor(3, 5) == 1: ' +
+  (greatest_common_factor(3, 5) == 1).to_s
+)
