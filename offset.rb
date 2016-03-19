015abbf054d9891631d9baf8ecaac1f83a665e01
Nikolai <nikolaicalder1@gmail.com>
2016-03-19 06:11:31 -0400

    added median solution and sting concat with inject

--- offset.rb
+++ offset.rb
@@ -6,3 +6,17 @@ def hash_correct(hash)
 end
 wrong_hash = { :a => "banana", :b => "cabbage", :c => "dental_floss", :d => "eel_sushi" }
 hash_correct(wrong_hash)
+
+
+def median(array)                          
+    array = array.sort                     
+    if array.length.odd?                   
+      return array[(array.length - 1) / 2] 
+    else array.length.even?                
+      return ( array[array.length/2] + array[array.length/2 - 1] )/2.to_f
+    end
+end
+
+def concat(arr)
+	arr.inject("") { |a,b| a << b}
+end
